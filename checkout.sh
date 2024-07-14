a070e4cbd26447a81719d01474c9cc0ba6dda4e3
jespinales <juliocesarespinales@gmail.com>
2024-07-14 18:47:26 -0500

    Update custom checkout. Add "&& \"

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-php ./vendor/bin/phinx rollback -e development
-git checkout "$@"
+php ./vendor/bin/phinx rollback -e development && \
+git checkout "$@" && \
 php ./vendor/bin/phinx migrate -e development
